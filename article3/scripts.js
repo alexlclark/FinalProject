5000f66d8938d04b93ccb07cc08f1608585919fc
alexlclark <alexclarkcontact@gmail.com>
2015-03-25 18:48:29 -0400

    Committing files from existing project

---
+++ article3/scripts.js
@@ -0,0 +1,19 @@
+/**
+ * @author Alex Lewis Clark
+ */
+var map = L.map('leafletMap').setView([39.906081, -73.965308], 13);
+
+// add an OpenStreetMap tile layer
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+	attribution : '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+var shelters = shelterData.rows;
+
+for (var i = 0; i < shelters.length; i++) {
+
+	var individualWarmingShelters = shelters[i];
+
+	var marker = L.marker([individualWarmingShelters.latitude, individualWarmingShelters.longitude]).addTo(map);
+	marker.bindPopup("<b>" + individualWarmingShelters.FullAddress + "</b><br>This location is in the following borough:" + "<br>" + individualWarmingShelters.Borough + "</b><br>The zipcode for this location is:" + "<br>" + individualWarmingShelters.ZipCode).openPopup();
+} // end of for loop!! 
